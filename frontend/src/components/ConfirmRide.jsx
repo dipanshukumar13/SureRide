f37bbc409a44195003276dd511c50dc0cbb5d019
dipanshukumar13 <dipanshukragrawal2003@gmail.com>
2025-06-13 03:15:25 +0530

    confirmride updated

--- frontend/src/components/ConfirmRide.jsx
+++ frontend/src/components/ConfirmRide.jsx
@@ -4,7 +4,7 @@ const ConfirmRide = () => {
   return (
     <div>
       <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
-        props.setVehiclePanel(false)
+        props.setConfirmRidePanel(false)
       }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
 
       <h3 className='text-2xl font-semibold mb-5'>Confirm your Ride</h3>
@@ -33,7 +33,12 @@ const ConfirmRide = () => {
               </div>
           </div>
        </div>
-       <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button>
+       <button
+       onClick={()=>{
+        props.setConfirmRidePanel(false)
+        props.setVehicleFound(true)
+       }}
+        className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button>
       </div>
     </div>
   )
