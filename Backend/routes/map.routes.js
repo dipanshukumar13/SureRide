82f133f7a0854e273af59cc1c8002ee3ec921ec3
dipanshukumar13 <dipanshukragrawal2003@gmail.com>
2025-06-19 21:31:06 +0530

    maps.routes done

---
+++ Backend/routes/map.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const mapController = require('../controllers/map.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+const {query} = require('express-validator');
+
+router.get('/get-coordinates',
+    query('address').isString().isLength({min:3}),
+    authMiddleware.authUser,mapController.getCoordinates
+);
+
+module.exports = router;
