634a540be980e3b76eb24b457628b86cfb1a7d51
dipanshukumar13 <dipanshukragrawal2003@gmail.com>
2025-06-19 22:25:38 +0530

    GetDistance and AutoSuggestion controller Added

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -17,3 +17,36 @@ module.exports.getCoordinates=async(req,res,next)=>{
         res.status(404).json({message:'Coordinates not found'});
     }
 }
+
+module.exports.getDistanceTime=async(req,res,next)=>{
+    try{
+        const errors=validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+        const {origin, destination} = req.query;
+        const distanceTime = await mapService.getDistanceTime(origin, destination);
+        res.status(200).json(distanceTime);
+    }
+    catch(error){
+        res.status(404).json({message:'Distance and time not found'});
+    }
+}
+
+module.exports.getAutoCompleteSugestions=async(req,res,next)=>{
+    try{
+        const errors=validationResult(req);
+        if(!errors.isEmpty()){
+            throw res.status(400).json({errors:errors.array()});
+        }
+        const {input} = req.query;
+        const suggestions=await mapService.getAutoCompleteSuggestions(input);
+        res.status(200).json(suggestions);
+    }
+    catch(error){
+        console.error(error);
+        res.status(404).json({message:'Internal Server Error'});
+    }   
+}
+
+
